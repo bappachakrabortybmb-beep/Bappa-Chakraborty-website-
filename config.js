6e03281709f58a23f9a7702c704c1e54f5f92c38
bappachakrabortybmb-beep <bappachakrabortybmb@gmail.com>
2026-08-19 19:03:36 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Supabase project settings
+// 1) Create a Supabase project.
+// 2) Put your Project URL and anon/public key below.
+// NEVER put a service_role key in this file.
+export const SUPABASE_URL = 'PASTE_YOUR_SUPABASE_PROJECT_URL';
+export const SUPABASE_ANON_KEY = 'PASTE_YOUR_SUPABASE_ANON_KEY';
