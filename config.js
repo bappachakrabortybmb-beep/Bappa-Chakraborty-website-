1eea4a7a9b2d13c8bfbbb6ea1900f9b269c8d448
bappachakrabortybmb-beep <bappachakrabortybmb@gmail.com>
2026-08-19 20:23:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // 1) Create a Supabase project.
 // 2) Put your Project URL and anon/public key below.
 // NEVER put a service_role key in this file.
-export const SUPABASE_URL = 'PASTE_YOUR_SUPABASE_PROJECT_URL';
-export const SUPABASE_ANON_KEY = 'PASTE_YOUR_SUPABASE_ANON_KEY';
+export const SUPABASE_URL = 'https://psmxnkraimevcwnbdztx.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzbXhua3JhaW1ldmN3bmJkenR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxMzc3MTQsImV4cCI6MjEwMjcxMzcxNH0.df-itkgp5rlKonnhzAQwBxnKucI7922j19531tkVdq8';
